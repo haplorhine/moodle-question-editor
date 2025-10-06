97d8c2dedd309d83df5a5621514489e4866b1690
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2025-10-06 14:22:02 +0200

    Add import button

--- src/components/ImportFile.jsx
+++ src/components/ImportFile.jsx
@@ -1,10 +1,33 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 const ImportFile = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = ({ target }) => {
+    const targetFile = target.files[0] ? target.files[0] : null;
+    setSelectedFile(targetFile);
+  };
+
+  const handleImport = () => {
+    if (!selectedFile) {
+      return;
+    }
+  };
+
   return (
     <Form.Group controlId="formFile" className="mb-3">
       <Form.Label>Default file input example</Form.Label>
-      <Form.Control type="file" />
+      <Form.Control type="file" onChange={handleFileChange} />
+      <Button
+        className="mt-2"
+        variant="primary"
+        onClick={handleImport}
+        disabled={!selectedFile}
+      >
+        Import
+      </Button>
     </Form.Group>
   );
 };
