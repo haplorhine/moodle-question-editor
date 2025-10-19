cf699ce1fc68e43ba92beabe65d846f3d2326777
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2025-10-19 20:47:21 +0200

    Add question component

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,12 +1,41 @@
+import Form from 'react-bootstrap/Form';
+
+
+const Question = ({ question }) => {
+  console.log(question)
+  return (
+    <div className="rounded border bg-warning p-3">
+      <Form>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Question Name</Form.Label>
+          <Form.Control value={question.name.text} type="text" placeholder="" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Question Text</Form.Label>
+          <Form.Control value={question.questiontext.text} as="textarea" rows={3} />
+        </Form.Group>
+      </Form>
+
+    </div>
+  )
+
+}
+
+
+
+
+
+
+
 const Questions = ({ filter, questions }) => {
 
   if (!questions) {
     return null
-  } 
+  }
 
-  
 
-  const questionList = questions.map(question => <pre key={question.uuid} className="rounded border bg-warning p-3">{JSON.stringify(question, null, 2)}</pre>)
+
+  const questionList = questions.map(question => <Question key={question.uuid} question={question} />)
 
   return questionList
 
