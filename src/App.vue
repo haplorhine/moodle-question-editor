ae4f8ac52f575109301fe67881681c8d2bf8fa78
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2025-10-25 23:16:42 +0200

    Setup vue project with router

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Router from './components/Router.vue'
+</script>
+
+<template>
+  <HelloWorld msg="Geschafft!" />
+  <Router />
+</template>
+
+<style scoped></style>
