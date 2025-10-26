9fb16297f0e3928e48e0f87d497ac2b697026261
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2025-10-26 21:41:57 +0100

    Add code fragments to test vue logic and create custom event

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <script setup>
+import { ref } from 'vue'
 import FileImport from './components/FileImport.vue'
 import HelloWorld from './components/HelloWorld.vue'
 import QuestionEditor from './components/QuestionEditor.vue'
 import Searchbar from './components/Searchbar.vue'
 
-const data = true
+const parsedXML = ref(null)
+const filter = ref('')
+
+console.log(parsedXML.value)
+
+const handleImport = (file) => console.log('handle file: ', file.value)
 </script>
 
 <template>
   <h1>Moodle Question Editor</h1>
 
-  <FileImport />
-  <Searchbar v-if="data" />
+  <FileImport @response="(file) => handleImport(file)" />
+  <Searchbar v-if="parsedXML" />
 
   <QuestionEditor questions="{filteredQuestions}" />
 </template>
