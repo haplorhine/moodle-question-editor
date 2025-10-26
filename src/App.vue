19d6158680cac7f80bb642f69c7c8c4f8ac71d62
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2025-10-26 23:27:21 +0100

    Implement raw print of questions

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,41 @@
 <script setup>
 import { ref } from 'vue'
+import { XMLParser } from 'fast-xml-parser'
 import FileImport from './components/FileImport.vue'
-import HelloWorld from './components/HelloWorld.vue'
 import QuestionEditor from './components/QuestionEditor.vue'
 import Searchbar from './components/Searchbar.vue'
 
 const parsedXML = ref(null)
 const filter = ref('')
 
+const parseFile = (text) => {
+  // XMLParser options
+  const options = {
+    ignoreAttributes: false,
+  }
+  const parser = new XMLParser(options)
+
+  const jsonObj = parser.parse(text)
+  // debugger;
+
+  // uuid um vue ein key attribute zu geben bei iteration
+  jsonObj.quiz.question = jsonObj.quiz.question.map((question) => ({
+    ...question,
+    uuid: crypto.randomUUID(),
+    answer: question.answer?.map((answer) => ({
+      ...answer,
+      uuid: crypto.randomUUID(),
+    })),
+  }))
+  parsedXML.value = jsonObj
+}
+
 const handleImport = (file) => {
   if (!file) {
     return
   }
 
-  console.log('handle file: ', file)
+  file.text().then((text) => parseFile(text))
 }
 </script>
 
@@ -22,6 +44,9 @@ const handleImport = (file) => {
 
   <FileImport @importClick="(file) => handleImport(file)" />
   <Searchbar v-if="parsedXML" />
+
+  <!-- <p v-for="question of parsedXML.data"><pre>{{ JSON.stringify(question) }}</pre></p> -->
+  <pre v-if="parsedXML">{{ JSON.stringify(parsedXML.quiz.question, null, 4) }}</pre>
 
   <QuestionEditor questions="{filteredQuestions}" />
 </template>
