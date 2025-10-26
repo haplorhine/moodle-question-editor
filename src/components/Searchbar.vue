0c2b54c264b8b92218e437cd2e460d7843578a56
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2025-10-27 00:33:16 +0100

    Wrap content

--- src/components/Searchbar.vue
+++ src/components/Searchbar.vue
@@ -3,9 +3,11 @@ import { ref } from 'vue'
 const searchTerm = ref('')
 </script>
 <template>
-  <label for="searchTerm">Search Question:</label>
-  <input v-model="searchTerm" type="text" id="searchTerm" name="searchTerm" />
-  <span v-if="searchTerm">Questions containing: {{ searchTerm }}</span>
-  <span v-else>All questions</span>
+  <div>
+    <label for="searchTerm">Search Question:</label>
+    <input v-model="searchTerm" type="text" id="searchTerm" name="searchTerm" />
+    <span v-if="searchTerm">Questions containing: {{ searchTerm }}</span>
+    <span v-else>All questions</span>
+  </div>
 </template>
 <style scoped></style>
