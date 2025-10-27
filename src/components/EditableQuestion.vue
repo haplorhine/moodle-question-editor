f17a70fab31a24148a723d1e671bf9eb7ae53f53
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2025-10-28 00:19:21 +0100

    Change to standard input field

--- src/components/EditableQuestion.vue
+++ src/components/EditableQuestion.vue
@@ -7,7 +7,7 @@ defineProps(['question'])
 <template>
   <div class="questionEditing">
     <p>Name:</p>
-    <TextEditor v-model:html="question.name.text" />
+    <input type="text" v-model="question.name.text" width="1000" />
     <p>Text:</p>
     <TextEditor v-model:html="question.questiontext.text" />
   </div>
@@ -18,5 +18,8 @@ defineProps(['question'])
   border: 2px solid red;
   padding: 1em;
   margin: 1em;
+}
+.questionEditing input {
+  width: 500px;
 }
 </style>
