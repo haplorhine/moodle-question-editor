7b941b1db88ac295c329e4e992d53665008cf4ec
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2025-12-14 20:39:28 +0100

    Add Button component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,26 @@
+<script setup>
+const props = defineProps({
+  text: {
+    type: String,
+    default: 'Button',
+  },
+  title: {
+    type: String,
+    default: '',
+  },
+})
+
+const emit = defineEmits(['click'])
+
+const handleClick = () => {
+  emit('click')
+}
+</script>
+
+<template>
+  <div class="d-flex justify-content-center mb-3">
+    <button class="btn btn-primary btn-lg" @click="handleClick" :title="title">
+      {{ text }}
+    </button>
+  </div>
+</template>
