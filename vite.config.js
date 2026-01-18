41738954fb8e2b7ffdca179fb7eff5fc34d0c1e5
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2026-01-18 12:07:04 +0100

    Add vite-pwa to simplify generation and enable offline use

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,38 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'icons/*'],
+      manifest: {
+        name: 'Moodle Question Editor',
+        short_name: 'MoodleQEditor',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#1976d2',
+        icons: [
+          {
+            src: 'icons/icon192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/icon512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
